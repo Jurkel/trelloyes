3a6e6c40255ffbcdd3627b0a7ac077d9867cfc41
Harvey Le <harveyxcle@gmail.com>
2020-02-26 14:15:26 -0500

    testing card and list

---
+++ src/List.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './List';
+
+it('renders without crashing', () => {
+  const section = document.createElement('section');
+  ReactDOM.render(<List />, section);
+  ReactDOM.unmountComponentAtNode(section);
+});

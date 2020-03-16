4f219ec22637f18c0f7529c47823bc2ff62ec717
Harvey Le <harveyxcle@gmail.com>
2020-03-16 18:15:35 -0400

    updating state with callback props

--- src/List.js
+++ src/List.js
@@ -14,9 +14,11 @@ function List(props) {
             key={card.id}
             title={card.title}
             content={card.content}
+            onDeleteCard={props.onDeleteCard}
           />
         )}
         <button 
+          onClick={props.onRandomCard}
           type='button' 
           className='List-add-button'
         >
